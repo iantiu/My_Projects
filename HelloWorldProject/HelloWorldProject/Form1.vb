251325df1dd5739b82c94c95b07fc9eef4fa0732
Seth Adam <seth.adam@dakotasoftworks.com>
2017-09-12 12:09:18 -0500

    Implementing Hello World button
    Along with an out of spec feature in order to help illustrate my coding style... not that performing out-of-spec work is typically my style ;)

--- HelloWorldProject/HelloWorldProject/Form1.vb
+++ HelloWorldProject/HelloWorldProject/Form1.vb
@@ -1,6 +1,30 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        TextBox1.Text = "": textbox1.focus
+        TextBox1.Text = "Hello World"
+
+        textbox1.TextAlign = HorizontalAlignment.Center
+        TextBox1.BackColor = ProvideRandomColor()
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Text = ""
+        textbox1.focus
+
+        textbox1.TextAlign = HorizontalAlignment.Left
+        TextBox1.BackColor = Color.White
+    End Sub
+
+    Private Function ProvideRandomColor() As Color
+        Dim rand = new Random
+
+        Dim red = rand.Next(0, 255)
+        Dim green = rand.Next(0, 255)
+        Dim blue = rand.Next(0, 255)
+
+        Const alpha = 255
+
+        Return Color.FromArgb(alpha, red, green, blue)
+    End Function
 End Class
